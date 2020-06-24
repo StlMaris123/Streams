1bc31f9f377580a1e8c96448104201410e8d1c73
StlMaris123 <stellamarisnjage@gmail.com>
2020-06-24 18:35:28 +0200

    update user state upon signin/out

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,16 +1,33 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    state = { isSignedIn: null }
     componentDidMount(){
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '713393290322-b6mr180f74hn4vqkm70novqg94i4am35.apps.googleusercontent.com',
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({isSignedIn: this.auth.isSignedIn.get()});
+                this.auth.isSignedIn.listen(this.onAuthChange);
             })
         })
     }
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get()})
+    }
+    renderAuthButton(){
+        if (this.state.isSignedIn === null){
+            return <div>I don't know if I am signed In</div>
+        }else if (this.state.isSignedIn) {
+            return <div>I am signed In</div>
+        }else {
+            return <div>I am not signed in</div>
+        }
+    }
     render(){
-        return <div>GoogleAuth</div>
+        return <div>{ this.renderAuthButton() }</div>
     }
 }
 
